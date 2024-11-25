77d47d11bde992761c4bcaad912e2da89e1a8bb7
Lentinel <113000289+Lentinel@users.noreply.github.com>
2024-11-25 10:19:39 +0800

    Update DUT_TeacherEvaluation_Auto_Submit_Tool.js

--- DUT_TeacherEvaluation_Auto_Submit_Tool.js
+++ DUT_TeacherEvaluation_Auto_Submit_Tool.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         大连理工教师评教问卷自动提交工具
 // @namespace    https://github.com/Lentinel/DUT_TeacherEvaluation_Auto_Submit_Tool
-// @version      1.0
+// @version      1.1
 // @description  DUT_TeacherEvaluation_Auto_Submit_Tool
 // @author       Lentinel
 // @match        *://jxgl.dlut.edu.cn/evaluation-student-frontend/*
@@ -39,7 +39,7 @@
     }
 
     setTimeout(function () {
-        showAutoCloseAlert('请不要在 按任务评教 - 学生端评教系统 点击按钮，需要进入指定教师评教页面后点击“点我评教”', 5000);
+        showAutoCloseAlert('请在进入教师评教问卷页面后再点击“点我评教”按钮。', 5000);
 
         const containerDiv = doc.querySelector('div[class="main-container"]');
         if (!containerDiv) return;
@@ -58,6 +58,7 @@
                 "您会向学弟学妹推荐该老师的课程吗？": "是",
                 "教师讲解（理论课）": "老师讲解问题非常清晰透彻，重点非常突出。",
                 "教师讲解": "老师注重因材施教，对我指导非常到位。",
+                "优秀",
                 "引导帮助": "老师非常善于启发我的思维，总是对我的问题给予及时、有帮助的反馈，让我对这门课程产生浓厚兴趣。",
                 "课程内容": "课程内容对我非常有用，课程所学对我今后学习、工作和生活有很大价值。",
                 "教学设计": "教学设计精心，教学组织非常好，师生互动非常活跃，老师讲课或指导有方。",
@@ -75,16 +76,34 @@
                     radio.click();
                 }
             });
-            // 滚动到底部
-            window.scrollTo(0, document.body.scrollHeight);
-            // 填写评论
+
+            setTimeout(function() {
+        const scoreInput = doc.querySelector('input.el-input__inner[placeholder="请选择"]');
+        if (scoreInput) {
+            scoreInput.click(); 
+
+            setTimeout(function() {
+                const scoreOption = doc.querySelector('ul.el-scrollbar__view > li.el-select-dropdown__item:last-child');
+                if (scoreOption) {
+                    scoreOption.click();
+                    console.log('已选择 100 分');
+                } else {
+                    console.error('评分选项未找到');
+                }
+            }, 200); // 根据页面渲染速度调整延迟时间
+        } else {
+            console.error('评分输入框未找到');
+        }
+    }, 2000); // 根据页面加载速度调整延迟时间
+            
             const textareaElement = doc.querySelector('textarea');
             if (textareaElement && textareaElement.value === "") {
                 textareaElement.value = "非常喜欢这门课！";
                 textareaElement.dispatchEvent(new InputEvent("input"));
             }
 
-            // 自动点击提交
+            window.scrollTo(0, document.body.scrollHeight);
+            
             setTimeout(function () {
                 const submitButton = doc.querySelector('button.el-button--primary');
                 if (submitButton) {
@@ -94,5 +113,5 @@
             }, 300);
         });
 
-    }, 2000);
+    }, 3000);
 })(window, document);
